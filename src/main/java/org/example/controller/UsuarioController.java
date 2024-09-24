dd9ae1df52554b6be738dad34568c30ce5f5cc83
Guilherme <guilherme.menezescr@gmail.com>
2024-09-24 02:22:04 -0300

    teste

--- src/main/java/org/example/controller/UsuarioController.java
+++ src/main/java/org/example/controller/UsuarioController.java
@@ -25,6 +25,8 @@ public class UsuarioController {
     @PostMapping("/cadastrar")
     public ResponseEntity<UsuarioEntity> cadastrarUsuario(@RequestBody UsuarioDTO usuarioDTO){
 
+
+
         UsuarioEntity novoUsuario = repository.save(usuarioDTO.toEntity());
 
         return ResponseEntity.status(HttpStatus.CREATED).body(novoUsuario);
